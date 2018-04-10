d3a803eb54d58aa26d318cc156376bea69890354
Shajan Jacob <shajanjp@gmail.com>
2018-04-10 15:09:34 +0530

    lints code

--- script.js
+++ script.js
@@ -1,69 +1,71 @@
-let offStateColor = "ff4444";
-let onStateColor = "#22ff77";
-let loadingStateColor = "#ffdd66";
+let offStateColor = 'ff4444';
+let onStateColor = '#22ff77';
+let loadingStateColor = '#ffdd66';
 
-var makeSwitch = function(device, status){
-	if(status == 0)
-		$(device).find('.image svg g path').css({ fill: offStateColor });
-	else if(status == 1)
-		$(device).find('.image svg g path').css({ fill: onStateColor });
-	else
-		$(device).find('.image svg g path').css({ fill: loadingStateColor });
+let makeSwitch = function(device, status) {
+  if (status == 0) {
+$(device).find('.image svg g path').css({fill: offStateColor});
+} else if (status == 1) {
+$(device).find('.image svg g path').css({fill: onStateColor});
+} else {
+$(device).find('.image svg g path').css({fill: loadingStateColor});
 }
+};
 
-var nextState = function(currentState){
-	if (currentState == 1) 
-		return 0;
-	else
-		return 1;
+let nextState = function(currentState) {
+  if (currentState == 1) {
+return 0;
+} else {
+return 1;
 }
+};
 
-var checkDeviceStatus = function(){
-	$.get("http://" + $('#mcuip').val() + "/devices", function(data) {
-		if(data){
-			data.forEach(function(d){
-				makeSwitch(d.device, d.status);
-			});
-		}
-	});
-}
+let checkDeviceStatus = function() {
+  $.get('http://' + $('#mcuip').val() + '/devices', function(data) {
+    if (data) {
+      data.forEach(function(d) {
+        makeSwitch(d.device, d.status);
+      });
+    }
+  });
+};
 
-$('.device').on('click', function(){
-	let current_device = $(this);
-	let data = {};
-	data.device = current_device.data('device');
-	data.status = nextState(current_device.data('state'));
-	makeSwitch(this, 1);
-	$.get("http://" + $('#mcuip').val() + "/devices", data, function(response){
-		makeSwitch(response.device, response.state);
-	});
+$('.device').on('click', function() {
+  let currentDevice = $(this);
+  let data = {};
+  data.device = currentDevice.data('device');
+  data.status = nextState(currentDevice.data('state'));
+  makeSwitch(this, 1);
+  $.get('http://' + $('#mcuip').val() + '/devices', data, function(response) {
+    makeSwitch(response.device, response.state);
+  });
 });
 
-$('.color').on('click', function(){
-	let currentColor = $(this);
-	let effect = $('input[name=effect]:checked').val();
-	let colorRGB = {};
-	colorRGB.r = currentColor.data('r');
-	colorRGB.g = currentColor.data('g');
-	colorRGB.b = currentColor.data('b');
-	colorRGB.effect = effect;
-	$.get("http://" + $('#mcuip').val() + "/strip", colorRGB, function(response){
-		console.log("Color changed !");
-	});
+$('.color').on('click', function() {
+  let currentColor = $(this);
+  let effect = $('input[name=effect]:checked').val();
+  let colorRGB = {};
+  colorRGB.r = currentColor.data('r');
+  colorRGB.g = currentColor.data('g');
+  colorRGB.b = currentColor.data('b');
+  colorRGB.effect = effect;
+  $.get('http://' + $('#mcuip').val() + '/strip', colorRGB, function(response) {
+    console.log('Color changed !');
+  });
 });
 
-$('#check-device-status').on('click', function(){
-	checkDeviceStatus();
+$('#check-device-status').on('click', function() {
+  checkDeviceStatus();
 });
 
-$('#connect-device').on('click', function(){
-	checkDeviceStatus();
+$('#connect-device').on('click', function() {
+  checkDeviceStatus();
 });
 
-$('#turn-off-devices').on('click', function(){
-	$.get("http://" + $('#mcuip').val() + "/devices?device=all&status=0", function(data) {
-		data.forEach(function(d){
-			makeSwitch(d.device, d.status);
-		});
-	});
+$('#turn-off-devices').on('click', function() {
+  $.get('http://' + $('#mcuip').val() + '/devices?device=all&status=0', function(data) {
+    data.forEach(function(d) {
+      makeSwitch(d.device, d.status);
+    });
+  });
 });
