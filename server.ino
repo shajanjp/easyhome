e76146867526c03eb8e210503cfd86e7ba184a3e
Shajan Jacob <shajanjacob@wimoku.com>
2021-01-15 19:35:20 +0530

    fix: adds support for ir remote

--- server.ino
+++ server.ino
@@ -2,16 +2,21 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <IRrecv.h>
 
-const char* ssid = "RedAnt"; // wifi SSID
-const char* password = "0n3st3p@h3@d"; // wifi password
+uint16_t RECV_PIN = D4; // GPIO 2
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+const char *ssid = "Redant";          // wifi SSID
+const char *password = "hellogwc404"; // wifi password
 
 ESP8266WebServer server(80);
 
 #include <NeoPixelBus.h>
- 
-const uint16_t PixelCount = 30; // this example assumes 4 pixels, making it smaller will cause a failure
-const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266 and takes RX pin by default
+
+const uint16_t PixelCount = 24; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 2;     // make sure to set this to the correct pin, ignored for Esp8266 and takes RX pin by default
 
 #define colorSaturation 255
 
@@ -19,7 +24,7 @@ const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignore
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
 
 // Switches d1, d2, d5, d6 || 5, 4, 14, 12
-int devices[] = {5,4,14,12};
+int devices[] = {5, 4, 14, 12};
 int device_status[4];
 
 // For rainbow
@@ -28,22 +33,26 @@ uint8_t pos;
 RgbColor black(0, 0, 0);
 
 // Home API
-void handleRoot(){
+void handleRoot()
+{
   server.send(200, "application/json", "{ \"msg\": \"Welcome to EasyHome\", \"version\": \"1.05 Beta\" }");
 }
 
 // 404
-void handleNotFound(){
+void handleNotFound()
+{
   server.send(404, "application/json", "{ \"msg\": \"Invalid API request\" }");
 }
 
 // Strip Success
-void handleStripSuccess(){
+void handleStripSuccess()
+{
   server.send(200, "application/json", "{ \"msg\": \"Enjoy the colors\" }");
 }
 
 // Set all PINS ON or OFF
-void setThese(int start, int end, RgbColor myColor){
+void setThese(int start, int end, RgbColor myColor)
+{
   for (int i = start; i <= end; ++i)
   {
     strip.SetPixelColor(i, myColor);
@@ -51,16 +60,17 @@ void setThese(int start, int end, RgbColor myColor){
   strip.Show();
 }
 
-void rainbow(){
-  for(uint16_t j=0; j<256*5; j++) // complete 5 cycles around the color wheel
-  { 
-    for(uint16_t i=0; i<PixelCount; i++)
+void rainbow()
+{
+  for (uint16_t j = 0;; j++) // complete 5 cycles around the color wheel
+  {
+    for (uint16_t i = 0; i < PixelCount; i++)
     {
       // generate a value between 0~255 according to the position of the pixel
       // along the strip
-      pos = ((i*256/PixelCount)+j) & 0xFF;
+      pos = ((i * 256 / PixelCount) + j) & 0xFF;
       // calculate the color for the ith pixel
-      color = Wheel( pos );
+      color = Wheel(pos);
       // set the color of the ith pixel
       strip.SetPixelColor(i, color);
     }
@@ -70,7 +80,8 @@ void rainbow(){
   }
 }
 
-void runThrough(int start, int end, RgbColor myColor, int speed, int count) {
+void runThrough(int start, int end, RgbColor myColor, int speed, int count)
+{
   for (int c = 0; c < count; ++c)
   {
     for (int i = start; i <= end; ++i)
@@ -91,43 +102,48 @@ void runThrough(int start, int end, RgbColor myColor, int speed, int count) {
   }
 }
 
-
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-RgbColor Wheel(uint8_t WheelPos) 
+RgbColor Wheel(uint8_t WheelPos)
 {
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) 
+  if (WheelPos < 85)
   {
     return RgbColor(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) 
+  }
+  else if (WheelPos < 170)
   {
     WheelPos -= 85;
     return RgbColor(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else 
+  }
+  else
   {
     WheelPos -= 170;
     return RgbColor(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } 
+  }
 }
 
 // Swithing devices
-void turnDevice(int device, int state){
-  if(state == 0){
+void turnDevice(int device, int state)
+{
+  if (state == 0)
+  {
     digitalWrite(devices[device], LOW);
     device_status[device] = 0;
   }
-  else if(state == 1){
+  else if (state == 1)
+  {
     digitalWrite(devices[device], HIGH);
     device_status[device] = 1;
   }
 }
 
 // Handle deivce requests
-void handleDevice(int device, int state){
+void handleDevice(int device, int state)
+{
   turnDevice(device, state);
   String response = "{\"device\":";
-  response += (device + 1); 
+  response += (device + 1);
   response += ", \"status\":";
   response += device_status[device];
   response += "}";
@@ -136,7 +152,9 @@ void handleDevice(int device, int state){
 }
 
 // Main Setup
-void setup(void) {
+void setup(void)
+{
+  irrecv.enableIRIn();
 
   pinMode(5, OUTPUT);
   digitalWrite(5, LOW);
@@ -156,7 +174,8 @@ void setup(void) {
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -167,25 +186,27 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("ESP8266")) {
+  if (MDNS.begin("ESP8266"))
+  {
     Serial.println("MDNS responder started...");
   }
 
   server.on("/", handleRoot);
 
-  server.on("/rainbow", [](){ 
+  server.on("/rainbow", []() {
     server.send(200, "application/json", "{ \"msg\": \"Enjoy rainbow colors\" }");
     return rainbow();
   });
 
-  server.on("/strip", [](){
-    if(server.arg("r") != "" && server.arg("g") !="" && server.arg("b") != "") {
+  server.on("/strip", []() {
+    if (server.arg("r") != "" && server.arg("g") != "" && server.arg("b") != "")
+    {
       handleStripSuccess();
-      if(server.arg("effect") == "roll")
+      if (server.arg("effect") == "roll")
         return runThrough(0, PixelCount - 1, RgbColor(server.arg("r").toInt(), server.arg("g").toInt(), server.arg("b").toInt()), 10, 3);
-      if(server.arg("effect") == "snake")
+      if (server.arg("effect") == "snake")
         return runThrough(0, PixelCount - 1, RgbColor(server.arg("r").toInt(), server.arg("g").toInt(), server.arg("b").toInt()), 100, 1);
-      if(server.arg("effect") == "flat")
+      if (server.arg("effect") == "flat")
         return setThese(0, PixelCount - 1, RgbColor(server.arg("r").toInt(), server.arg("g").toInt(), server.arg("b").toInt()));
       else
         return setThese(0, PixelCount - 1, RgbColor(server.arg("r").toInt(), server.arg("g").toInt(), server.arg("b").toInt()));
@@ -195,8 +216,10 @@ void setup(void) {
   });
 
   server.on("/devices", []() {
-    if(server.args() == 2) {
-      if(server.arg("device").toInt() && server.arg("status")) {
+    if (server.args() == 2)
+    {
+      if (server.arg("device").toInt() && server.arg("status"))
+      {
         handleDevice(server.arg("device").toInt() - 1, server.arg("status").toInt());
       }
       else
@@ -210,8 +233,104 @@ void setup(void) {
   Serial.println("HTTP server started...");
   strip.Begin();
   strip.Show();
+  runThrough(0, PixelCount - 1, RgbColor(200, 200, 200), 10, 1);
 }
 
-void loop(void){
+void remoteAction(int code)
+{
+  switch (code)
+  {
+  case 16748655: // red
+    setThese(0, PixelCount - 1, RgbColor(255, 0, 0));
+    break;
+
+  case 16716015: // green
+    setThese(0, PixelCount - 1, RgbColor(0, 255, 0));
+    break;
+
+  case 16732335: // blue
+    setThese(0, PixelCount - 1, RgbColor(0, 0, 245));
+    break;
+
+  case 255: // white
+    setThese(0, PixelCount - 1, RgbColor(200, 200, 200));
+    break;
+
+  case 16756815: // orange
+    setThese(0, PixelCount - 1, RgbColor(255, 165, 0));
+    break;
+
+  case 16724175: // light green
+    setThese(0, PixelCount - 1, RgbColor(144, 238, 144));
+    break;
+
+  case 16740495: // dark blue
+    setThese(0, PixelCount - 1, RgbColor(0, 0, 139));
+    break;
+
+  case 16754775: // dark yellow
+    setThese(0, PixelCount - 1, RgbColor(246, 109, 48));
+    break;
+
+  case 16722135: // cyan
+    setThese(0, PixelCount - 1, RgbColor(0, 255, 255));
+    break;
+
+  case 16738455: // lyon blue
+    setThese(0, PixelCount - 1, RgbColor(0, 191, 255));
+    break;
+
+  case 16750695: // yellow
+    setThese(0, PixelCount - 1, RgbColor(247, 155, 40));
+    break;
+
+  case 16718055: // light blue
+    setThese(0, PixelCount - 1, RgbColor(173, 216, 230));
+    break;
+
+  case 16734375: // purple
+    setThese(0, PixelCount - 1, RgbColor(128, 0, 128));
+    break;
+
+  case 16746615: // light yellow
+    setThese(0, PixelCount - 1, RgbColor(255, 255, 0));
+    break;
+
+  case 16713975: // skyblue
+    setThese(0, PixelCount - 1, RgbColor(135, 206, 235));
+    break;
+
+  case 16730295: // red dish
+    setThese(0, PixelCount - 1, RgbColor(255, 20, 147));
+    break;
+
+  case 16762935: // rainbow
+    rainbow();
+    break;
+
+  case 16736415: // black
+    setThese(0, PixelCount - 1, RgbColor(0, 0, 0));
+    break;
+
+  case 16769055: // white animation
+    runThrough(0, PixelCount - 1, RgbColor(200, 200, 200), 10, 1);
+    break;
+
+  default:
+    setThese(0, PixelCount - 1, RgbColor(255, 255, 255));
+    break;
+  }
+}
+
+void loop(void)
+{
   server.handleClient();
+  if (irrecv.decode(&results))
+  {
+    unsigned int ircode = results.value;
+    Serial.println(ircode);
+    remoteAction(ircode);
+    irrecv.resume();
+  }
+  delay(100);
 }
